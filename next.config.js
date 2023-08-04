367c0e2b7e1776dfccebbfb9b92ad233e4a6adb9
hovinee <hobinskyyy@naver.com>
2023-08-05 00:32:28 +0900

    refactor: domaim 변경 마지막

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   images: {
     domains: [
       'firebasestorage.googleapis.com',
-      'https://www.koreanbeefricesoup.com/'
+      'koreanbeefricesoup.com/'
     ],
   },
   webpack: (config) => {
