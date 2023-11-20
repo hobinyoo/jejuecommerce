a9863cfb51a684195f87bf3c9cc614cafb4aa10c
hobinyoo <hobinskyyy@naver.com>
2023-11-20 21:23:53 +0900

    우편크기 테스트

--- src/components/modal/PostModal.tsx
+++ src/components/modal/PostModal.tsx
@@ -48,6 +48,7 @@ const PostModal = ({ setPostVisible, setAddress, setPostCode }: Props) => {
           }} // 값을 선택할 경우 실행되는 이벤트
           autoClose={false} // 값을 선택할 경우 사용되는 DOM을 제거하여 자동 닫힘 설정
           defaultQuery="판교역로 235" // 팝업을 열때 기본적으로 입력되는 검색어
+          style={{ width: '100%', height: '100%' }}
         />
       </div>
     </div>
